601fbe0c1d205055a689f6af996a33710439fd7f
Best <hollyjanebest@gmail.com>
2024-12-30 13:50:34 +0000

    fixing files

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import { StatusBar} from 'expo-status-bar';
 import React from 'react';
 import { View } from 'react-native';
 import { GameEngine } from 'react-native-game-engine';
+import entities from './entities';
+import Physics from './physics';
 
 export default function App(){
     return(
         <View style = {{flex: 1}}>
             <GameEngine
-            entities={entities()}
+            systems = {[Physics]}
+                entities={entities()}
                 style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0}}
             >
             </GameEngine>
