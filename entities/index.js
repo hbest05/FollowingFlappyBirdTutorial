ef8964f38435bdbe25343ee4990da4f9d0be1161
Best <hollyjanebest@gmail.com>
2024-12-31 00:32:56 +0000

    chapter 6 of tutorial

--- entities/index.js
+++ entities/index.js
@@ -1,5 +1,11 @@
 import Matter from "matter-js"
 import Bird from "../components/bird";
+import Floor from "../components/Floor";
+
+import { Dimensions } from 'react-native';
+
+const windowHeight = Dimensions.get('window').height
+const windowWidth = Dimensions.get('window').width
 
 export default restart => {
     let engine = Matter.Engine.create({enableSleeping: false})
@@ -10,6 +16,8 @@ export default restart => {
 
     return{
         physics: {engine, world},
-        Bird: Bird(world, 'green', {x: 50, y:300}, {height: 40, width: 40})
+        Bird: Bird(world, 'green', {x: 50, y:300}, {height: 40, width: 40}),
+        Floor: Floor(world, 'green', {x: windowWidth/2, y: windowHeight}, {height: 50, width: windowWidth})
+        
     }
 }
