f00c6b86222499fb53b60d44bd83132bb7dae65b
Best <hollyjanebest@gmail.com>
2025-01-01 12:49:04 +0000

    finished tutorial

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar} from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
-import { View } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import { GameEngine } from 'react-native-game-engine';
 import entities from './App';
 import Physics from './physics';
@@ -10,10 +10,11 @@ export default function App(){
     const [gameEngine, setGameEngine] = useState(null)
     const [currentPoints, setCurrentPoints] = useState(0)
     useEffect(() => {
-        setRunning(true)
+        setRunning(false)
     }, [])
     return(
         <View style = {{flex: 1}}>
+            <Text style ={{textAlign: 'center', fontSize: 40, fontWeight: 'bold', margin: 20}}>{currentPoints}</Text>
             <GameEngine
                 ref = {(ref) => {setGameEngine(ref)}}
                 systems = {[Physics]}
@@ -24,7 +25,6 @@ export default function App(){
                         case 'game_over':
                             setRunning(false)
                             gameEngine.stop()
-                            setCurrentPoints(0)
                             break;
                         case 'new_point':
                             setCurrentPoints(currentPoints + 1)
@@ -33,9 +33,22 @@ export default function App(){
                 }}
                 style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0}}
             >
+                <StatusBar style = "auto" hidden={true}/>
             </GameEngine>
             
-            <StatusBar style = "auto" hidden={true}/>
+            {!running ?
+                <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                    <TouchableOpacity style = {{backgroundColor: 'black', paddingHorizontal: 30, paddingVertical: 10}}
+                        onPress = {() => {
+                            setCurrentPoints(0)
+                            setRunning(true)
+                            gameEngine.swap(entities)
+                        }}>
+                        <Text style = {{fontWeight: 'bold', color: 'white', fontSize: 30}}>
+                            START GAME
+                        </Text>
+                    </TouchableOpacity>
+                </View> : null } 
         </View>
     );
 }
